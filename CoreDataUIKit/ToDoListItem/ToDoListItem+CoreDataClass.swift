d49a28054cbc7e6fc9b1ef48cb1b586b8273fd2c
cynomac <cynomac@cynomacs-Mac-mini.local>
2021-10-07 18:53:55 +0530

    This is a UIkit ToDo list app.

---
+++ CoreDataUIKit/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataUIKit
+//
+//  Created by cynomac on 07/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
